cf3b47fd79436867d76b4b6ffe3836fad4598fc9
Balls <abdullahtoor326@gmail.com>
2026-01-05 01:53:10 +0000

    User.java foundational requirements finished student.java started

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student extends User {
+    private String uniID;
+    private int studyYear;
+
+    public Student(String id, String name, String email, String uniID, int studyYear) {
+        super(id, name, email);
+        this.uniID = uniID;
+        this.studyYear = studyYear;
+    }
+
+    public String getUniID()
+    
+}
---
+++ User.java
@@ -0,0 +1,14 @@
+public abstract class User {
+    private String id; 
+    private String name; 
+    private String email;
+    
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+}
+
+
